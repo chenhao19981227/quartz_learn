660af3f981c19f7aec490b06413be3a46940d748
ch <956718871@qq.com>
2024-04-18 13:28:51 +0800

    " "

---
+++ src/main/java/com/ch/quartz_learn/QuartzLearnApplication.java
@@ -0,0 +1,13 @@
+package com.ch.quartz_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ch/quartz_learn/QuartzLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ch.quartz_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
