48cf03e402057936a2d88436b6d7831915645c63
ch <956718871@qq.com>
2024-04-21 12:01:55 +0800

    quartz：注入bean service类

---
+++ src/main/java/com/ch/quartz_learn/service/HelloService.java
@@ -0,0 +1,10 @@
+package com.ch.quartz_learn.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+    public String helloService(){
+        return "Hello";
+    }
+}
