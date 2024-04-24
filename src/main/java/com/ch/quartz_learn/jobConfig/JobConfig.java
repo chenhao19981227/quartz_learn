414ee303cefdc1d7955791dc1a53c5ec343b33be
ch <956718871@qq.com>
2024-04-24 12:45:31 +0800

    quartz：持久化 以Bean的方式注入数据源

--- src/main/java/com/ch/quartz_learn/jobConfig/JobConfig.java
+++ src/main/java/com/ch/quartz_learn/jobConfig/JobConfig.java
@@ -6,22 +6,37 @@ import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.Trigger;
 import org.quartz.TriggerBuilder;
+import org.springframework.boot.autoconfigure.quartz.QuartzDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
 @Configuration
 public class JobConfig {
     @Bean
-    public JobDetail springJobDetail(){
-        return JobBuilder.newJob(SpringBeanJob2.class)
-                .withIdentity("springJobDetail")
-                .storeDurably()
-                .build();
+    @QuartzDataSource
+    public DataSource qDatasource(){
+        DriverManagerDataSource dataSource= new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUsername("root");
+        dataSource.setPassword("123456");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/quartz_learn?useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC");
+        return dataSource;
     }
-    @Bean
-    public Trigger springJobTrigger(){
-        return TriggerBuilder.newTrigger()
-                .forJob("springJobDetail")
-                .startNow()
-                .build();
-    }
+//    @Bean
+//    public JobDetail springJobDetail(){
+//        return JobBuilder.newJob(SpringBeanJob2.class)
+//                .withIdentity("springJobDetail")
+//                .storeDurably()
+//                .build();
+//    }
+//    @Bean
+//    public Trigger springJobTrigger(){
+//        return TriggerBuilder.newTrigger()
+//                .forJob("springJobDetail")
+//                .startNow()
+//                .build();
+//    }
 }
